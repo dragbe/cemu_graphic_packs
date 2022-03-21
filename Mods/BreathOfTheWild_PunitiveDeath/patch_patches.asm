f2c9a5aac0504f582ae29f33c6e166add61d1efd
dragbe <58640146+dragbe@users.noreply.github.com>
2022-03-21 12:07:55 +0100

    Add files via upload

---
+++ Mods/BreathOfTheWild_PunitiveDeath/patch_patches.asm
@@ -0,0 +1,992 @@
+[BotwV208]
+moduleMatches = 0x6267BFD0
+
+.origin = codecave
+
+_BOTW_PunitiveDeath_WeaponId = 0x10187F9C
+_BOTW_PunitiveDeath_PorchItemDataHashValue:
+.int 1596469897
+_BOTW_PunitiveDeath_PorchItemValue1DataHashValue:
+.int 1779039321
+_BOTW_PunitiveDeath_PorchItemEquipFlagDataHashValue:
+.int -2109173058
+_BOTW_PunitiveDeath_PorchSwordFlagSpDataHashValue:
+.int 1475224093
+_BOTW_PunitiveDeath_PorchShieldFlagSpDataHashValue:
+.int -987525845
+_BOTW_PunitiveDeath_PorchBowFlagSpDataHashValue:
+.int 213845290
+_BOTW_PunitiveDeath_PorchSwordValueSpDataHashValue:
+.int -1495718212
+_BOTW_PunitiveDeath_PorchShieldValueSpDataHashValue:
+.int 1777434250
+_BOTW_PunitiveDeath_PorchBowValueSpDataHashValue:
+.int 507499156
+# Target data arrays loading order
+# PorchBow_FlagSp 0cbf052a s32_array_data
+# PorchBow_ValueSp 1e3fd294 s32_array_data
+# PorchSword_FlagSp 57ee221d s32_array_data
+# PorchItem 5f283289 string64_array_data
+# PorchShield_ValueSp 69f17e8a s32_array_data
+# PorchItem_Value1 6a09fc59 s32_array_data
+# PorchItem_EquipFlag 824892be bool_array_data
+# PorchSword_ValueSp a6d926bc s32_array_data
+# PorchShield_FlagSp c5238d2b s32_array_data
+_BOTW_PunitiveDeath_S32ArrayDataProperties:
+.int 0 # S32 arrays count
+.int 0 # PorchBow_FlagSp array address
+.int 0 # PorchBow_FlagSp array length
+.int 2 # PorchBow_FlagSp related item type
+.int 0
+.int 0 # PorchBow_ValueSp array address
+.int 0 # PorchBow_ValueSp array length
+.int 2 # PorchBow_ValueSp related item type
+.int 0
+.int 0 # PorchSword_FlagSp array address
+.int 0 # PorchSword_FlagSp array length
+.int 1 # PorchSword_FlagSp related item type
+.int 0
+_BOTW_PunitiveDeath_Weaponsmap:
+.int 0 # Dump map
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0 # Swords count|Filtered swords count
+.int 0 # Filtered swords 1-based indexes
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0 # Bows count|Filtered bows count
+.int 0 # Filtered bows 1-based indexes
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0 # Shields count|Filtered shields count
+.int 0 # Filtered shields 1-based indexes
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+.int 0
+_BOTW_PunitiveDeath_EssentialItemsFilters:
+.int $EssentialItems
+.int 12 # [1] Master Sword filter: Item ID LIKE "............_070.*"
+.int 1596995376
+.int 1596995376
+.int 1
+.int 12 # [2] Moon Slayer filter: Item ID LIKE "............_Lyl.*"
+.int 1598847340
+.int 1598847340
+.int 1
+.int -4 # [4] Normal|ice|fire|electric|ancien arrows filter: Item ID LIKE ".*rrow$"
+.int 1920102263
+.int 1920102263
+.int 1
+.int 2 # [8] Bomb arrows filter: Item ID LIKE ".*mbAr.*"
+.int 1835155826
+.int 1835155826
+.int 1
+.int -4 # [16] Normal|ice|fire|electric|ancien arrows exclusion filter: Item ID LIKE ".*rrow$"
+.int 1920102263
+.int 1920102263
+.int -1
+.int 2 # [32] Bomb arrows exclusion filter: Item ID LIKE ".*mbAr.*"
+.int 1835155826
+.int 1835155826
+.int -1
+.int 6 # [64] Wild set filter: Item ID LIKE "......005_.*"
+.int 808465759
+.int 808465759
+.int 1
+.int 6 # [128] Wild set * filter: Item ID LIKE "......035_.*"
+.int 808662367
+.int 808662367
+.int 1
+.int 6 # [256] Wild set ** filter: Item ID LIKE "......039_.*"
+.int 808663391
+.int 808663391
+.int 1
+.int 6 # [512] Wild set ***[*] filter: Item ID LIKE "......06[0-1]_.*"
+.int 808857695
+.int 808857951
+.int 1
+.int 6 # [1024] Thunder Helm and Champion's Tunic filter: Item ID LIKE "......11[5-6]_.*"
+.int 825308511
+.int 825308767
+.int 1
+.int 6 # [2048] Champion's Tunic *[*][*][*] filter: Item ID LIKE "......[148-151]_.*"
+.int 825505887
+.int 825569631
+.int 1
+.int 9 # [4096] Ancient Materials filter: Item ID LIKE ".........y_[26-31].*"
+.int 2036281910
+.int 2036282161
+.int 1
+.int 2 # [8192] Boiled|Cooked|Frozen|Icy foods filter: Item ID LIKE "..em_[B-C].*"
+.int 1701666626
+.int 1701666627
+.int 1
+.int 2 # [16384] Roasted|Blackened|Seared|Baked|Toasted|Charred foods filter: Item ID LIKE "..em_R.*"
+.int 1701666642
+.int 1701666642
+.int 1
+.int 2 # [32768] Materials filter: Item ID LIKE "..em_[E-P].*"
+.int 1701666629
+.int 1701666640
+.int 1
+.int 10 # [65536] Wood filter: Item ID LIKE "..........odBu.*"
+.int 1868841589
+.int 1868841589
+.int 1
+.int 0 # [131072] Courser Bee Honey filter: Item ID LIKE "^BeeH.*"
+.int 1113941320
+.int 1113941320
+.int 1
+.int 2 # [262144] Boiled|Cooked|Frozen|Icy foods exclusion filter: Item ID LIKE "..em_[B-C].*"
+.int 1701666626
+.int 1701666627
+.int -1
+.int 2 # [524288] Roasted|Blackened|Seared|Baked|Toasted|Charred foods exclusion filter: Item ID LIKE "..em_R.*"
+.int 1701666642
+.int 1701666642
+.int -1
+.int 2 # [1048576] Materials exclusion filter: Item ID LIKE "..em_[E-P].*"
+.int 1701666629
+.int 1701666640
+.int -1
+.int 10 # [2097152] Wood exclusion filter: Item ID LIKE "..........odBu.*"
+.int 1868841589
+.int 1868841589
+.int -1
+.int 0 # [4194304] Courser Bee Honey exclusion filter: Item ID LIKE "^BeeH.*"
+.int 1113941320
+.int 1113941320
+.int -1
+.int 0 # [8388608] Armor filter: Item ID LIKE "^Armo.*"
+.int 1098018159
+.int 1098018159
+.int 1
+.int 0 # [16777216] Non-weapons items filter: Item ID NOT LIKE "^Weap.*"
+.int 1466261872
+.int 1466261872
+.int 0
+.int 0 # [33554432] Weapons filter: Item ID LIKE "^Weap.*"
+.int 1466261872
+.int 1466261872
+.int 1
+
+_BOTW_PunitiveDeath_IsAlive:
+.int 1
+_BOTW_PunitiveDeath_PorchItemValuesMap:
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1
+.int -1 # PorchItemValues[420] always -1
+
+_BOTW_PunitiveDeath_PatchS32ArrayGameData:
+li r16, -1
+stw r16, 0x4(r19)
+addi r24, r24, 3
+lis r12, _BOTW_PunitiveDeath_S32ArrayDataProperties@ha
+lwzu r16, _BOTW_PunitiveDeath_S32ArrayDataProperties@l(r12)
+stw r3, 0x0(r12)
+addi r16, r16, -1
+addi r12, r12, 16
+lwz r17, -0xC(r12)
+lwz r5, -0x8(r12)
+lwz r18, -0x4(r12)
+slwi r18, r18, 6
+add r18, r18, r24
+lhz r14, -0x1(r18)
+cmpwi r14, 0
+beq .+0x2C
+cmpw r5, r14
+bge .+0x8
+mr r14, r5
+mr r15, r17
+mtctr r14
+lbzu r19, 0x1(r18)
+slwi r19, r19, 5
+lwzx r19, r19, r15
+stwu r19, 0x20(r17)
+bdnz .-0x10
+sub r5, r5, r14
+cmpwi r5, 0
+beq .+0x10
+mtctr r5
+stwu r3, 0x20(r17)
+bdnz .-0x4
+cmpwi r16, 0
+bne .-0x6C
+blr
+
+_BOTW_PunitiveDeath_LoadStringArrayGameData:
+lwz r10, 0x92C(r31)
+lis r19, _BOTW_PunitiveDeath_IsAlive@ha
+lwzu r3, _BOTW_PunitiveDeath_IsAlive@l(r19)
+cmpwi r3, 1
+beqlr
+lis r17, _BOTW_PunitiveDeath_PorchItemDataHashValue@ha
+lwz r17, _BOTW_PunitiveDeath_PorchItemDataHashValue@l(r17)
+lwz r16, 0x0(r12)
+cmpw r17, r16
+bnelr
+lis r15, _BOTW_PunitiveDeath_EssentialItemsFilters@ha
+lwzu r14, _BOTW_PunitiveDeath_EssentialItemsFilters@l(r15)
+mulli r16, r7, 320
+addi r12, r12, -64
+sub r16, r12, r16
+mr r7, r16
+lis r8, _BOTW_PunitiveDeath_WeaponId@ha
+lwz r8, _BOTW_PunitiveDeath_WeaponId@l(r8)
+lis r24, _BOTW_PunitiveDeath_Weaponsmap@ha
+addi r24, r24, _BOTW_PunitiveDeath_Weaponsmap@l
+addi r16, r16, 320
+lwzu r17, 0x140(r7)
+rlwinm r18, r17, 16, 16, 31
+cmpwi r18, 16754 # Ar
+addi r29, r24, 4
+beq .+0x48
+lhz r18, 0x4(r7)
+cmpwi r18, 17490 # DR
+beq .+0xBC
+cmpw r17, r8
+bne .+0x34
+lhz r18, 0x7(r7)
+cmpwi r18, 17007 # Bo 0100001001101111
+beq .+0x10
+cmpwi r18, 21352 # Sh 0101001101101000
+beq .+0x8
+li r18, 256
+rlwinm r18, r18, 24, 30, 31
+slwi r18, r18, 6
+add r29, r29, r18
+lhz r18, -0x4(r29)
+addi r18, r18, 1
+sth r18, -0x4(r29)
+mr r20, r14
+mr r6, r15
+addi r6, r6, 16
+rlwinm r18, r20, 0, 31, 31
+rlwinm r20, r20, 31, 1, 31
+cmpwi r18, 1
+bne .-0x10
+lwz r18, -0xC(r6)
+cmpwi r18, 0
+bge .+0x18
+mr r5, r7
+lbzu r4, 0x1(r5)
+addi r18, r18, 1
+cmpwi r4, 0
+bne .-0xC
+lwzx r5, r18, r7
+lwz r18, -0x8(r6)
+cmpw r5, r18
+li r4, 0
+blt .+0x14
+lwz r18, -0x4(r6)
+cmpw r5, r18
+bgt .+0x8
+li r4, 1
+lwz r18, 0x0(r6)
+rlwinm r5, r18, 0, 31, 31
+cmpw r5, r4
+beq .+0x90
+cmpwi r20, 0
+bne .-0x6C
+stb r3, 0x0(r7)
+b .-0xD4
+sub r18, r7, r16
+stwu r18, 0x4(r19)
+cmpwi r18, 0
+beq .+0x24
+stw r17, 0x0(r16)
+li r3, 60
+lwzx r17, r3, r7
+stwx r17, r3, r16
+cmpwi r3, 4
+addi r3, r3, -4
+bne .-0x10
+stb r3, 0x0(r7)
+lwzu r17, 0x140(r7)
+cmpw r7, r12
+bgt _BOTW_PunitiveDeath_PatchS32ArrayGameData
+cntlzw r18, r17
+cmpwi r18, 7
+bgt _BOTW_PunitiveDeath_PatchS32ArrayGameData
+addi r16, r16, 320
+sub r18, r7, r16
+stwu r18, 0x4(r19)
+cmpwi r18, 0
+beq .-0x28
+stw r17, 0x0(r16)
+li r3, 60
+lwzx r17, r3, r7
+stwx r17, r3, r16
+cmpwi r3, 4
+addi r3, r3, -4
+bne .-0x10
+b .-0x4C
+cmpwi r18, 0
+blt .-0x88
+sth r3, 0x2(r24)
+lhz r18, -0x4(r29)
+lhz r6, -0x2(r29)
+stbx r18, r6, r29
+addi r6, r6, 1
+sth r6, -0x2(r29)
+sub r18, r7, r16
+stwu r18, 0x4(r19)
+cmpwi r18, 0
+beq .-0x184
+stw r17, 0x0(r16)
+li r3, 60
+lwzx r17, r3, r7
+stwx r17, r3, r16
+cmpwi r3, 4
+addi r3, r3, -4
+bne .-0x10
+stb r3, 0x0(r7)
+b .-0x1A8
+
+_BOTW_PunitiveDeath_LoadS32ArrayGameData:
+lwz r10, 0x92C(r31)
+lis r19, _BOTW_PunitiveDeath_IsAlive@ha
+lwzu r3, _BOTW_PunitiveDeath_IsAlive@l(r19)
+cmpwi r3, 1
+beqlr
+lwz r12, 0x8(r1)
+lis r17, _BOTW_PunitiveDeath_PorchSwordFlagSpDataHashValue@ha
+lwz r17, _BOTW_PunitiveDeath_PorchSwordFlagSpDataHashValue@l(r17)
+cmpw r17, r12
+li r18, 1
+beq .+0x14
+lis r17, _BOTW_PunitiveDeath_PorchSwordValueSpDataHashValue@ha
+lwz r17, _BOTW_PunitiveDeath_PorchSwordValueSpDataHashValue@l(r17)
+cmpw r17, r12
+bne .+0xB0
+lwz r17, 0x84(r27)
+lwzx r17, r17, r22
+lwz r17, 0x8(r17)
+lwz r17, 0x0(r17)
+addi r17, r17, -12
+lwz r16, 0x4(r19)
+cmpwi r16, -1
+beq .+0x68
+lis r24, _BOTW_PunitiveDeath_Weaponsmap@ha
+addi r24, r24, _BOTW_PunitiveDeath_Weaponsmap@l
+addi r24, r24, 3
+slwi r18, r18, 6
+add r18, r18, r24
+lhz r14, -0x1(r18)
+cmpwi r14, 0
+beq .+0x2C
+cmpw r5, r14
+bge .+0x8
+mr r14, r5
+mr r15, r17
+mtctr r14
+lbzu r19, 0x1(r18)
+slwi r19, r19, 5
+lwzx r19, r19, r15
+stwu r19, 0x20(r17)
+bdnz .-0x10
+sub r5, r5, r14
+cmpwi r5, 0
+beqlr
+mtctr r5
+stwu r3, 0x20(r17)
+bdnz .-0x4
+blr
+lis r3, _BOTW_PunitiveDeath_S32ArrayDataProperties@ha
+lwzu r16, _BOTW_PunitiveDeath_S32ArrayDataProperties@l(r3)
+addi r16, r16, 1
+stw r16, 0x0(r3)
+slwi r16, r16, 4
+add r3, r3, r16
+stw r17, -0xC(r3)
+stw r5, -0x8(r3)
+stw r18, -0x4(r3)
+blr
+lis r17, _BOTW_PunitiveDeath_PorchShieldFlagSpDataHashValue@ha
+lwz r17, _BOTW_PunitiveDeath_PorchShieldFlagSpDataHashValue@l(r17)
+cmpw r17, r12
+li r18, 3
+beq .-0xBC
+lis r17, _BOTW_PunitiveDeath_PorchShieldValueSpDataHashValue@ha
+lwz r17, _BOTW_PunitiveDeath_PorchShieldValueSpDataHashValue@l(r17)
+cmpw r17, r12
+beq .-0xCC
+lis r17, _BOTW_PunitiveDeath_PorchBowFlagSpDataHashValue@ha
+lwz r17, _BOTW_PunitiveDeath_PorchBowFlagSpDataHashValue@l(r17)
+cmpw r17, r12
+li r18, 2
+beq .-0xE0
+lis r17, _BOTW_PunitiveDeath_PorchBowValueSpDataHashValue@ha
+lwz r17, _BOTW_PunitiveDeath_PorchBowValueSpDataHashValue@l(r17)
+cmpw r17, r12
+beq .-0xF0
+lis r17, _BOTW_PunitiveDeath_PorchItemValue1DataHashValue@ha
+lwz r17, _BOTW_PunitiveDeath_PorchItemValue1DataHashValue@l(r17)
+cmpw r17, r12
+bnelr
+li r16, 10
+lwz r17, 0x84(r27)
+lwzx r17, r17, r22
+lwz r17, 0x8(r17)
+lwz r17, 0x0(r17)
+addi r17, r17, -12
+addi r5, r5, -1
+addi r17, r17, 32
+lwzu r12, 0x4(r19)
+cmpwi r12, -1
+beq .+0x1C
+divw r12, r12, r16
+lwzx r12, r12, r17
+stw r12, 0x0(r17)
+cmpwi r5, 0
+bne .-0x24
+blr
+stw r3, 0x0(r17)
+cmpwi r5, 0
+beqlr
+addi r5, r5, -1
+stwu r3, 0x20(r17)
+b .-0x10
+
+_BOTW_PunitiveDeath_LoadBoolArrayGameData:
+lwz r10, 0x92C(r31)
+lis r19, _BOTW_PunitiveDeath_IsAlive@ha
+lwzu r3, _BOTW_PunitiveDeath_IsAlive@l(r19)
+cmpwi r3, 1
+beqlr
+lis r17, _BOTW_PunitiveDeath_PorchItemEquipFlagDataHashValue@ha
+lwz r17, _BOTW_PunitiveDeath_PorchItemEquipFlagDataHashValue@l(r17)
+lwz r12, 0x8(r1)
+cmpw r17, r12
+bnelr
+li r16, 20
+lwz r17, 0x78(r27)
+lwzx r17, r17, r22
+lwz r17, 0x8(r17)
+lwz r17, 0x0(r17)
+addi r17, r17, -9
+addi r7, r7, -1
+addi r17, r17, 16
+lwzu r12, 0x4(r19)
+cmpwi r12, -1
+beq .+0x1C
+divw r12, r12, r16
+lbzx r12, r12, r17
+stb r12, 0x0(r17)
+cmpwi r7, 0
+bne .-0x24
+blr
+stb r3, 0x0(r17)
+cmpwi r7, 0
+beqlr
+addi r7, r7, -1
+stbu r3, 0x10(r17)
+b .-0x10
+
+_BOTW_PunitiveDeath_Death:
+li r5, 0
+lis r3, _BOTW_PunitiveDeath_IsAlive@ha
+stw r5, _BOTW_PunitiveDeath_IsAlive@l(r3)
+blr
+
+_BOTW_PunitiveDeath_SaveGame:
+li r7, 1
+lis r8, _BOTW_PunitiveDeath_IsAlive@ha
+stw r7, _BOTW_PunitiveDeath_IsAlive@l(r8)
+blr
+
+_BOTW_PunitiveDeath_GameSaveLoadingCompleted:
+lmw r24, 0x10(r1)
+lis r18, _BOTW_PunitiveDeath_IsAlive@ha
+lwzu r19, _BOTW_PunitiveDeath_IsAlive@l(r18)
+cmpwi r19, 1
+beqlr
+li r20, -1
+stw r20, 0x4(r18)
+lis r18, _BOTW_PunitiveDeath_Weaponsmap@ha
+stwu r19, _BOTW_PunitiveDeath_Weaponsmap@l(r18)
+stw r19, 0x40(r18)
+stw r19, 0x80(r18)
+stw r19, 0xC0(r18)
+lis r20, $DataLayerMemoryBase
+lwz r18, 0x0(r20)
+cmpw r20, r18
+bnelr
+lwz r20, 0x54(r18)
+stw r19, 0x14(r20) # CurrentRupee
+lwz r20, 0x44(r18)
+stw r19, 0x14(r20) # CurrentMamo
+blr
+
+0x02D16CB0 = bla _BOTW_PunitiveDeath_Death # Link death [li r5, 0]
+0x03253EAC = bla _BOTW_PunitiveDeath_LoadStringArrayGameData # String array game data loading area [lwz r10, +0x92C(r31)]
+0x03253C84 = bla _BOTW_PunitiveDeath_LoadS32ArrayGameData # S32 array game data loading area [lwz r10, +0x92C(r31)]
+0x03253BCC = bla _BOTW_PunitiveDeath_LoadBoolArrayGameData # Bool array game data loading area [lwz r10, +0x92C(r31)]
+0x02e41358 = bla _BOTW_PunitiveDeath_SaveGame # Game save process area [li r7, 1]
+0x03254f3C = bla _BOTW_PunitiveDeath_GameSaveLoadingCompleted # Game save loading completed area [lmw r24, +0x10(r1)]
