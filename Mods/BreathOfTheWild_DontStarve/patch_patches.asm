2578e63e5d95753f0d07b48520ee05d069493765
dragbe <58640146+dragbe@users.noreply.github.com>
2022-03-31 15:41:46 +0200

    Add files via upload

--- Mods/BreathOfTheWild_DontStarve/patch_patches.asm
+++ Mods/BreathOfTheWild_DontStarve/patch_patches.asm
@@ -64,17 +64,17 @@ blr
 
 0x02D90CAC = bla _BOTW_DontStarve_UpdateStamina # Update stamina area [li r29, 1]
 0x02d8fbe4 = bla _BOTW_DontStarve_RecoverHeart # Heart auto recover area [lwz r12, +0xe8(r31)]
-0x02d8fbe8 = bgt .+0x134 # Same area as above [lwz r0, +0x664(r12)]
-0x02d8fbec = lwz r0, 0x664(r12) # Same area as above [mtctr r0]
-0x02d8fbf0 = mtctr r0 # Same area as above [mr r3, r31]
+0x02d8fbe8 = bgt .+0x134 # [lwz r0, +0x664(r12)]
+0x02d8fbec = lwz r0, 0x664(r12) # [mtctr r0]
+0x02d8fbf0 = mtctr r0 # [mr r3, r31]
 0x03201EDC = bla _BOTW_DontStarve_SetDataMgrFlag1 # Update flag related to samemgr|gamedatamgr when start sleep or open map|inventory|ingame weapons+runes selector (and briefly in other cases) [stb r11, +0x2f8(r12)]
 0x03201F5C = bla _BOTW_DontStarve_UnsetDataMgrFlag1 # Update flag related to samemgr|gamedatamgr when stop sleep or close map|inventory|weapons|ingame weapons+runes selector (and briefly in other cases) [stb r0, +0x2f8(r12)]
 0x021B7CDC = nop # Restore health after sleep area for all beds [bl 0x02D49D38]
 0x02D8FC60 = lis r10, _BOTW_DontStarve_HeartAutoRecoverIntervalMax@ha # Reset heart auto recover interval area [lwz r10, +0x0(r11)]
-0x02D8FC64 = lfs f11, _BOTW_DontStarve_HeartAutoRecoverIntervalMax@l(r10) # Same area as above [lfs f11, +0x20C(r10)]
+0x02D8FC64 = lfs f11, _BOTW_DontStarve_HeartAutoRecoverIntervalMax@l(r10) # [lfs f11, +0x20C(r10)]
 # Warning: 0x02D8FC4C = cmplwi r0, 0x15 always false ???
 0x02D8FCF4 = lis r12, 0x1000 # Apply heart auto recover area [lwz r12, +0x0(r10)]
-0x02D8FCFC = lfs f11, 0x05F0(r12) # Same area as above [lfs f11, +0x1ec(r12)]
+0x02D8FCFC = lfs f11, 0x05F0(r12) # [lfs f11, +0x1ec(r12)]
 0x023a3358 = cmpw r0, r0 # Restore health after sleep area for beds with yellow hearts bonus [cmpwi r0, 0]
 #Action/SetExtraEnergyOfPlayer 0x023a2990
 #Action/EventRecoverPlayerLife 0x021B7cc0
